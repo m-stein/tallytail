6225aed86062179d953d30d15ffba56e6665faae
Martin Stein <martin.stein.2023@gmail.com>
2026-05-14 15:38:38 +0200

    T29: Add precommit script and treat warnings as errors

---
+++ precommit.ps1
@@ -0,0 +1,11 @@
+$commands = @(
+    "cargo fmt",
+    "cargo clippy -p web_front_end --target wasm32-unknown-unknown",
+    "cargo clippy -p desktop_app --target x86_64-pc-windows-msvc",
+    "cargo clippy -p web_back_end --target x86_64-pc-windows-msvc"
+)
+
+foreach ($cmd in $commands) {
+    Write-Host "$cmd"
+    Invoke-Expression $cmd
+}
